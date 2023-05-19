5e53a64e51fb45c675fddcf5bd2363033ec0b582
Smaga Sasha <smaga.sasha@gmail.com>
2023-05-19 10:09:04 +0300

    models and routes - ready

---
+++ api/routes/auth.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("Crypto-js");
+const jwt = require("jsonwebtoken");
+
+
+// reg
+
+
+
+
+// log in
